6f85a66b26200b7d6d6a4670165512ab2b037db2
Beatriz Tsumoto <beatriz.tsumoto@aluno.senai.br>
2026-04-06 16:10:51 -0300

    feat: arruma treinoRoutes e server.js

--- src/routes/treinoRoutes.js
+++ src/routes/treinoRoutes.js
@@ -1,6 +1,23 @@
-const multer = require('multer');
-const TreinoController = require('../controllers/treinoController');
+import { Router } from 'express';
+import multer from 'multer';
+import * as TreinoController from '../controllers/treinoController.js';
 
-const upload = multer({ storage: multer.memoryStorage() });
+const router = Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.get('/catalogo', TreinoController.buscarTodos);
+
+router.get('/catalogo/:id', TreinoController.buscarPorId);
+
+router.post('/catalogo', TreinoController.criar);
+
+router.put('/catalogo/:id', TreinoController.atualizar);
+
+router.delete('/catalogo/:id', TreinoController.deletar);
 
 router.post('/catalogo/:id/foto', upload.single('foto'), TreinoController.uploadFoto);
+
+
+export default router;
